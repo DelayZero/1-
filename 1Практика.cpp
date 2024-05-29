695d7e2b9975a8aaaf551e5fd14676a2b042ce0a
Eugene <161845672+DelayZero@users.noreply.github.com>
2024-05-29 17:43:53 +0300

    Add files via upload

---
+++ 1Практика.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string multiplyStrings(string num1, string num2) {
+    int n = num1.size();
+    int m = num2.size();
+    string result(n + m, '0');
+
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = m - 1; j >= 0; j--) {
+            int mul = (num1[i] - '0') * (num2[j] - '0');
+            int sum = mul + (result[i + j + 1] - '0');
+            result[i + j + 1] = (sum % 10) + '0';
+            result[i + j] += sum / 10;
+        }
+    }
+
+    size_t startPos = result.find_first_not_of('0');
+    if (startPos != string::npos) {
+        return result.substr(startPos);
+    }
+
+    return "0";
+}
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    string num1 = "5";
+    string num2 = "12";
+
+    string result = multiplyStrings(num1, num2);
+
+    cout << "Результат Умножения: " << result << endl;
+
+    return 0;
+}
